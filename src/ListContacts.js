8d3e4dab8e57ec20af69d7dca3160813f837ab5c
BipinBaj <bipinjz@gmail.com>
2020-02-09 21:00:10 +1100

    update

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -18,7 +18,9 @@ function ListContacts (props) {
                         <p>{contact.handle}</p>
                         
                     </div>
-                    <button className='contact-remove'>
+                    <button
+                        onClick={() => props.onDeleteContact(contact)}
+                    className='contact-remove'>
                         Remove
                     </button>
                         
